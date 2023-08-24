16e99808cb9d9788db72ee922339804894ab9d21
kiruba <vkirubanandan007@gmail.com>
2023-08-24 22:54:38 +0530

    trigger check

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -16,6 +16,7 @@ def call(){
         stage("Quality Gate"){
           timeout(time: 1, unit: 'HOURS') {
               def qg = waitForQualityGate()
+              print "the quality gate ${qg}"
               if (qg.status != 'OK') {
                   error "Pipeline aborted due to quality gate failure: ${qg.status}"
               }
